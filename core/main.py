cf460f7a7a1d39298e19545e17c33f7254cc2ffd
Amin Moezzi <moezi79amin@gmail.com>
2026-09-18 16:25:48 +0330

    refactor routes names and some operations
    - Renamed routes to plural nouns
    - add search feature to query parameter

--- core/main.py
+++ core/main.py
@@ -1,7 +1,7 @@
 
-from fastapi import FastAPI, status, HTTPException, Body
+from fastapi import FastAPI, status, HTTPException, Body, Query
 from fastapi.responses import JSONResponse
-
+from typing import Optional
 app = FastAPI()
 
 costs_dict = {}
@@ -16,7 +16,7 @@ def root():
     )
 
 
-@app.post("/adding_cost/")
+@app.post("/costs")
 def add_cost(
         description: str = Body(),
         amount: float = Body()
@@ -35,12 +35,29 @@ def add_cost(
     return JSONResponse(content=new_cost, status_code=status.HTTP_201_CREATED)
 
 
-@app.get("/cost_names/")
-def retriev_costs_list():
+@app.get("/costs")
+def retriev_costs_list(search : Optional[str] = Query(
+    default=None,
+    description=" Enter a keyword to filter costs by description",
+    alias="search",
+    max_length= 50
+    
+)):
+    # adding dictionary comprehension for search
+    if search:
+        filtered_costs = {
+            cost_key : cost_value for cost_key, cost_value in costs_dict.items()
+            if search.lower() in cost_value["description"].lower()
+
+        }
+
+        return JSONResponse(content=filtered_costs, status_code=status.HTTP_200_OK)
+    
     return JSONResponse(content=costs_dict, status_code=status.HTTP_200_OK)
 
 
-@app.get("/cost_names/{cost_id}")
+
+@app.get("/costs/{cost_id}")
 def fetch_detailed_id(cost_id: int):
     if cost_id in costs_dict:
         return JSONResponse(content=costs_dict[cost_id], status_code=status.HTTP_200_OK)
@@ -51,7 +68,7 @@ def fetch_detailed_id(cost_id: int):
     )
 
 
-@app.put("/cost_names/{cost_id}/")
+@app.put("/costs/{cost_id}")
 def update_detailed_cost(
         cost_id: int,
         cost_description: str = Body(),
@@ -69,7 +86,7 @@ def update_detailed_cost(
     )
 
 
-@app.delete("/cost_names/{cost_id}/")
+@app.delete("/costs/{cost_id}")
 def delete_detailed_cost(cost_id: int):
     if cost_id in costs_dict:
         del costs_dict[cost_id]
